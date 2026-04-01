4a5dd61b8adf70a90c802cb82253325f97bf4454
oereor <unkn0wn___us3r@outlook.com>
2026-04-01 11:26:06 +0800

    Complete Q1 of hw03: expanding a binary tree as a linked-list.

---
+++ hw03/TreeExpand.cpp
@@ -0,0 +1,55 @@
+//
+// Created by unkn0 on 2026-04-01.
+//
+
+#include <iostream>
+
+struct Node {
+    int data;
+    int left;
+    int right;
+};
+
+Node* nodes;
+
+bool* isRoot;
+
+int n;
+int rootIndex = 0;
+
+void preOrder(Node root) {
+    std::cout << root.data << ' ';
+    if (root.left != 0) {
+        preOrder(nodes[root.left]);
+    }
+    if (root.right != 0) {
+        preOrder(nodes[root.right]);
+    }
+}
+
+int main() {
+    std::cin >> n;
+    nodes = new Node[n + 1];
+    isRoot = new bool[n + 1];
+    isRoot[0] = false;
+    for (int i = 1; i <= n; i++) {
+        isRoot[i] = true;
+    }
+    for (int i = 1; i <= n; i++) {
+        int left, right;
+        std::cin >> nodes[i].data >> left >> right;
+        nodes[i].left = left;
+        nodes[i].right = right;
+        isRoot[left] = false;
+        isRoot[right] = false;
+    }
+    for (int i = 1; i <= n; i++) {
+        if (isRoot[i]) {
+            rootIndex = i;
+            break;
+        }
+    }
+    preOrder(nodes[rootIndex]);
+    delete[] nodes;
+    delete[] isRoot;
+}
